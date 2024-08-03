51395e61d87970e6a13ed5265776f93bd5d709fd
noobapoo99 <apoorvtripathi99@gmail.com>
2024-08-04 01:20:56 +0530

    onAuth-in-app

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,29 @@
 //import Login from "./components/Login.jsx";
 //import "./layout.scss";
 
+import { useEffect } from "react";
 import Chat from "./components/chat/Chat.jsx";
 import Detail from "./components/detail/Detail.jsx";
 import List from "./components/list/List.jsx";
 import Login_1 from "./components/Login_1/login_1.jsx";
 import Notification from "./components/notification/notification.jsx";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase.js";
 //import Notification from "./components/notification/notification.jsx";
 //import Notification from "./components/notification/notification.jsx";
 //import HomePage from "./components/HomePage.jsx";
 
-function App() {
+const App = () => {
   const user = false;
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      console.log(user);
+    });
+    return () => {
+      unSub();
+    };
+  }, []);
   return (
     <div className="container_1">
       {user ? (
@@ -28,6 +40,6 @@ function App() {
       <Notification />
     </div>
   );
-}
+};
 
 export default App;
