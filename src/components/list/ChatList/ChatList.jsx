61289a56ff0e5a1988ca054822743ea4e6841c8b
noobapoo99 <apoorvtripathi99@gmail.com>
2024-08-04 23:05:33 +0530

    add_user-failed

--- src/components/list/ChatList/ChatList.jsx
+++ src/components/list/ChatList/ChatList.jsx
@@ -59,25 +59,30 @@ const ChatList = () => {
         <div
           className="item"
           key={chat.chatId}
-          onClick={() => handleSelect(chat)}
+          /* onClick={() => handleSelect(chat)}
           style={{
             backgroundColor: chat?.isSeen ? "transparent" : "#5183fe",
-          }}
+          }} */
         >
-          <img
-            src={
-              chat.user.blocked.includes(currentUser.id)
-                ? "./avatar.png"
-                : chat.user.avatar || "./avatar.png"
-            }
-            alt=""
-          />
+          <img src="./avatar.png" alt="" />
           <div className="texts">
-            <span>
-              {chat.user.blocked.includes(currentUser.id)
-                ? "User"
-                : chat.user.username}
-            </span>
+            <span>jon</span>
+            <p>{chat.lastMessage}</p>
+          </div>
+        </div>
+      ))}
+      {chats.map((chat) => (
+        <div
+          className="item"
+          key={chat.chatId}
+          /* onClick={() => handleSelect(chat)}
+          style={{
+            backgroundColor: chat?.isSeen ? "transparent" : "#5183fe",
+          }} */
+        >
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>jon</span>
             <p>{chat.lastMessage}</p>
           </div>
         </div>
